03ea9d0bd36ebd042d3fedd072f0cac4ff253940
AKURATHI SASIDHAR <22kt1a0595@gmail.com>
2025-10-06 21:11:49 +0530

    fix: Configure static files for production server
    Integrates the WhiteNoise library to resolve 404 errors for static assets on Render.

--- devcheathub/settings.py
+++ devcheathub/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -141,6 +142,9 @@ STATICFILES_DIRS = [
 
 # 2. Define the location where all static files will be collected for production
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# This tells Django to use WhiteNoise to serve files
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
